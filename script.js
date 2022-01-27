c560d1e20e08d9c2618a6331b00855e05c401eb9
reshma <reshmanoorain44@gmail.com>
2022-01-27 11:58:40 +0530

    commit

---
+++ script.js
@@ -0,0 +1,58 @@
+
+// Set time out
+
+// function add() {
+//     console.log("Hello");
+// }
+
+// setTimeout(add,5000);
+// console.log("Great Day");
+
+// Promise
+
+// let promise= new Promise(function(resolve,reject){
+//     if(1<4){
+//         resolve("resolved")
+//     }
+//     else{
+//         reject("rejected")
+//     }
+// });
+
+// promise.then(function(res){
+//     console.log(res);
+// })
+
+// promise.catch(function(rej){
+//     console.log(rej);
+// }) 
+
+// Axios api
+
+// import axios from 'axios';
+
+// function fetchProducts() {
+
+//     axios.get("https://fakestoreapi.com/products")
+
+//     .then(function(res){
+//         console.log(res);
+//     })
+//     .catch(function(err){
+//         console.log(err);
+//     })
+// }
+
+
+
+// fetch method
+
+// fetch("https://fakestoreapi.com/products")
+
+// .then(function(res){
+//     console.log(res);
+// })
+
+// .catch(function(err){
+//     console.log(err);
+// })
